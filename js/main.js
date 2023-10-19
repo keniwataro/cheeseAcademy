d4f417208f777313d3d20952dde0dae09932c397
keniwataro <keniwa30421@gmail.com>
2023-10-19 14:42:44 +0900

    チーズアカデミー

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// jsを記述する際はここに記載していく
+document.getElementById("cheak1").addEventListener("click", function(event){ 
+    document.getElementById("drawer_input").checked = false; 
+});
+
+document.getElementById("cheak2").addEventListener("click", function(event){ 
+    document.getElementById("drawer_input").checked = false; 
+});
+
+document.getElementById("cheak3").addEventListener("click", function(event){ 
+    document.getElementById("drawer_input").checked = false; 
+});
+
+document.getElementById("cheak4").addEventListener("click", function(event){ 
+    document.getElementById("drawer_input").checked = false; 
+});
+
+document.getElementById("cheak5").addEventListener("click", function(event){ 
+    document.getElementById("drawer_input").checked = false; 
+});
